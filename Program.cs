74ab78b7b6ccd5c9b1d9a17ef8f3f97895bbd927
Ariel Viera <ariel.viera@gmail.com>
2021-09-11 07:53:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Akka.Net_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
