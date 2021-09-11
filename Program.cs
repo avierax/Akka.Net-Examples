873d415afb54a8da1e4821b9a346f03b50b5ab16
Ariel Viera <ariel.viera@gmail.com>
2021-09-11 11:07:04 -0400

    Simple interaction

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Akka.Actor;
 
 namespace Akka.Net_Examples
 {
+    class Greet
+    {
+
+    }
+
+    class GreeterActor : ReceiveActor
+    {
+        public GreeterActor()
+        {
+            Receive<Greet>(greet =>
+            {
+                Console.WriteLine("Actor has been greeted");
+            });
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var actorSystem = ActorSystem.Create("MainActorSystem");
+            var actorRef = actorSystem.ActorOf<GreeterActor>("actorA");
+            actorRef.Tell(new Greet());
+            Console.ReadLine();
         }
     }
 }
